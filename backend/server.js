8164c5609b4d9ad7f2406d212094b6d409c98f1f
Pradum Sonkar <pradumsonkar9112@gmail.com>
2024-02-11 00:57:05 +0530

    link icon  commit

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,8 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://chatapp-ndtd.onrender.com",
     // credentials: true,
   },
 });
